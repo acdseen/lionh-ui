06552394d83f38da056e09d360f0588fc7e51249
zhonglinhong <1209654515@qq.com>
2020-08-19 17:46:42 +0800

    upload

---
+++ .storybook/main.js
@@ -0,0 +1,39 @@
+module.exports = {
+  stories: [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  addons: [
+    "@storybook/preset-create-react-app",
+		"@storybook/addon-actions",
+		"@storybook/addon-links",
+		"@storybook/addon-viewport",
+		"@storybook/addon-knobs",
+    { name: "@storybook/addon-docs", options: { configureJSX: true } },
+		"@storybook/addon-a11y",
+		"@storybook/addon-storysource",
+  ],
+  webpackFinal: async (config) => {
+		config.module.rules.push({
+			test: /\.(ts|tsx)$/,
+			use: [
+				{
+					loader: require.resolve("react-docgen-typescript-loader"),
+					options: {
+						shouldExtractLiteralValuesFromEnum: true,
+						propFilter: (prop) => {
+							if (prop.parent) {
+								return !prop.parent.fileName.includes(
+									"node_modules"
+								);
+							}
+							return true;
+						},
+					},
+				},
+			],
+		});
+		config.resolve.extensions.push(".ts", ".tsx");
+		return config;
+	},
+}
---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+export * from './components/Button';
+export * from './components/shared/global';
+export * from './components/shared/styles';
+export * from './components/shared/animation';
