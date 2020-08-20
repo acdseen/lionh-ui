5ed5c6596f0bc395f315a3545ea9ba9a13bf25e1
acdseen <guangtina@gmail.com>
2020-08-20 07:43:12 +0000

    Deploying to gh-pages from  @ e1d969590978f9f2c521632ff8aa7ad2c4555261 🚀

---
+++ precache-manifest.20bafebbbc1922d3ef2d4c47a8a7ae8c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b7a6cfa894b9effac34edc3e8baa95e",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.928d8bf36bc856ebea29.bundle.js"
+  },
+  {
+    "url": "runtime~main.928d8bf36bc856ebea29.bundle.js"
+  },
+  {
+    "url": "vendors~main.928d8bf36bc856ebea29.bundle.js"
+  },
+  {
+    "url": "vendors~main.928d8bf36bc856ebea29.bundle.js.LICENSE.txt"
+  }
+]);
