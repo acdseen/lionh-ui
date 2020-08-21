d8e3e1c8b0d144ef7e3272ea77998fc540f5f71c
zhonglinhong <1209654515@qq.com>
2020-08-21 18:09:43 +0800

    update

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -28,34 +28,34 @@ export interface IconProps extends PathProps {
 
 export function Icon(props: IconProps) {
   const { block, icon, color } = props;
-  return (
-    <Svg
-      data-testid="icon-svg"
-      viewBox="0 0 1024 1024"
-      width="20px"
-      height="20px"
-      block={block}
-      {...props}
-    >
-      <Path d={icons[icon]} color={color} data-testid="icon-path"></Path>
-    </Svg>
-  );
-  // return React.createElement(
-  //   Svg,
-  //   {
-  //     viewBox: '0 0 1024 1024',
-  //     width: '20px',
-  //     height: '20px',
-  //     block,
-  //     ...props,
-  //     'data-testid': 'icon-svg',
-  //   },
-  //   React.createElement(Path, {
-  //     d: icons[icon],
-  //     color,
-  //     'data-testid': 'icon-path',
-  //   })
+  // return (
+  //   <Svg
+  //     data-testid="icon-svg"
+  //     viewBox="0 0 1024 1024"
+  //     width="20px"
+  //     height="20px"
+  //     block={block}
+  //     {...props}
+  //   >
+  //     <Path d={icons[icon]} color={color} data-testid="icon-path"></Path>
+  //   </Svg>
   // );
+  return React.createElement(
+    Svg,
+    {
+      viewBox: '0 0 1024 1024',
+      width: '20px',
+      height: '20px',
+      block,
+      ...props,
+      'data-testid': 'icon-svg',
+    },
+    React.createElement(Path, {
+      d: icons[icon],
+      color,
+      'data-testid': 'icon-path',
+    })
+  );
 }
 Icon.defaultProps = {
   block: false,
